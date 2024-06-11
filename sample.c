a1d81d0eaee171db119d9551c75c460f022f86b5
aleville <51177908+aleville@users.noreply.github.com>
2024-06-11 12:01:28 -0400

    Update sample.c

--- sample.c
+++ sample.c
@@ -1,5 +1,6 @@
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
     
 #define BUFSIZE 256
     
@@ -7,8 +8,8 @@
 int main(int argc, char** argv) {
     // Ensure that the user supplied exactly one command line argument
     if (argc != 2) { 
-    fprintf(stderr, "Please provide the address of a file as an input.\n");
-    return -1;
+        fprintf(stderr, "Please provide the address of a file as an input.\n");
+        return -1;
     }
     char cmd[BUFSIZE] = "wc -c < ";
     strcat(cmd, argv[1]);
